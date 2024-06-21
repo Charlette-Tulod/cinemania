1c1e5b5b3d40153ad0777d1df396cca360b1a068
Charlette Tulod <charlettevibar@gmail.com>
2024-06-21 21:24:06 +0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,32 @@
 Rails.application.routes.draw do
-  root 'movies#index'
+  apipie
   resources :movies
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :movies, only: [:index, :show, :create, :update, :destroy] do
+        member do
+          post 'favorite', to: 'movies#favorite'
+          delete 'unfavorite', to: 'movies#unfavorite'
+        end
+      end
+
+      resources :users, only: [:index, :show] do
+        member do
+          get 'favorite_movies'
+        end
+      end
+
+      resources :registrations, only: [:create, :destroy] do
+        post 'confirm', on: :collection
+      end
+    
+      resources :sessions, only: [:create, :destroy]
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'movies#index'
 end
